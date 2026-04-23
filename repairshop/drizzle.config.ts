6cc6fd6d2ea7613414c2ccff1bb66d671da1874b
Darkchan <buihohaidang@gmail.com>
2026-04-23 15:27:02 +0700

    refactor: simplify drizzle configuration by removing unnecessary parameters

--- repairshop/drizzle.config.ts
+++ repairshop/drizzle.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from "drizzle-kit";
 
-export default defineConfig({
+export default {
     schema: "./src/db/schema.ts",
     out: "./src/db/migrations",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    }
-});
+    connectionString: process.env.DATABASE_URL!,
+} satisfies Config;
